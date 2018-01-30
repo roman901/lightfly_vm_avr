f3e08c936db7954ac030f690b97792a737328330
Roman <spark@uwtech.org>
2018-01-30 18:34:21 +0300

    Update readme

---
+++ build/CMakeFiles/lightfly_vm_avr.elf.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/spark/Projects/lightfly_vm_avr/lightfly_vm/lightfly_vm_lib/src/lf_executor.c" "/home/spark/Projects/lightfly_vm_avr/build/CMakeFiles/lightfly_vm_avr.elf.dir/lightfly_vm/lightfly_vm_lib/src/lf_executor.c.obj"
+  "/home/spark/Projects/lightfly_vm_avr/lightfly_vm/lightfly_vm_lib/src/lf_loader.c" "/home/spark/Projects/lightfly_vm_avr/build/CMakeFiles/lightfly_vm_avr.elf.dir/lightfly_vm/lightfly_vm_lib/src/lf_loader.c.obj"
+  "/home/spark/Projects/lightfly_vm_avr/lightfly_vm/lightfly_vm_lib/src/lf_opcodes.c" "/home/spark/Projects/lightfly_vm_avr/build/CMakeFiles/lightfly_vm_avr.elf.dir/lightfly_vm/lightfly_vm_lib/src/lf_opcodes.c.obj"
+  "/home/spark/Projects/lightfly_vm_avr/lightfly_vm/lightfly_vm_lib/src/lf_stack.c" "/home/spark/Projects/lightfly_vm_avr/build/CMakeFiles/lightfly_vm_avr.elf.dir/lightfly_vm/lightfly_vm_lib/src/lf_stack.c.obj"
+  "/home/spark/Projects/lightfly_vm_avr/src/errors.c" "/home/spark/Projects/lightfly_vm_avr/build/CMakeFiles/lightfly_vm_avr.elf.dir/src/errors.c.obj"
+  "/home/spark/Projects/lightfly_vm_avr/src/main.c" "/home/spark/Projects/lightfly_vm_avr/build/CMakeFiles/lightfly_vm_avr.elf.dir/src/main.c.obj"
+  "/home/spark/Projects/lightfly_vm_avr/src/opcodes.c" "/home/spark/Projects/lightfly_vm_avr/build/CMakeFiles/lightfly_vm_avr.elf.dir/src/opcodes.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lightfly_vm/lightfly_vm_lib/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
