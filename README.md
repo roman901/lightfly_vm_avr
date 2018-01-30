368b09e67973108e5e09b74a56cbedecc0fc9e50
Roman <spark@uwtech.org>
2018-01-30 18:35:55 +0300

    Remove build dir

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lightfly_vm_avr
 Lightweight register-based virtual machine runner for AVR architecture
+
+For VM specifications look [here](https://github.com/roman901/lightfly_vm/SPEC.md)
--- build/CMakeFiles/upload_lightfly_vm_avr.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/upload_lightfly_vm_avr"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/upload_lightfly_vm_avr.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
